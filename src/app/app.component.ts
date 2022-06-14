923744c80944d5ab697bfd648d9b4dec799f1ab5
JakobNeumueller <jakobmartinneumueller@gmail.com>
2022-06-14 09:44:03 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
